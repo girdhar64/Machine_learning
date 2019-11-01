47c6905da2a424dfa9372e30a11318e6b056adb8
Nishant Singh Rathore <nishantsinghrathore058@gmail.com>
2019-11-01 14:44:08 +0530

    Support Vector Regression Implementation

---
+++ svr_girdhar64.py
@@ -0,0 +1,40 @@
+# SVR regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Fitting SVR to the dataset
+from sklearn.svm import SVR
+regressor=SVR(kernel='rbf',gamma='auto')#gamma set explicitally
+regressor.fit(X,y)
+# Predicting a new result
+y_pred = regressor.predict(np.array[6.5])
+
+# Visualising theSVR results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (SVR Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+'''# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()'''
